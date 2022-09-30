f797c8bb874f5517d39f868f5830c5a6c1095e2c
DanielEsquivelReynoso <daniel.esquivelreynoso@gmail.com>
2022-09-30 10:04:24 -0700

    added data dashboard

--- src/components/DogHealthDataDashboard.jsx
+++ src/components/DogHealthDataDashboard.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import {useState, useEffect} from 'react';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../firebase-config';
 import Hourly from './Hourly.jsx';
 import Daily from './Daily.jsx';
 
 function DogHealthDataDashboard() {
+  const [dog, setDog] = useState({});
+  const dogDocRef = doc(db, 'dogs', 'pGHcLGLMS4OIstaj5ZKW');
+  getDoc(dogDocRef)
+    .then((doc) => {
+      console.log(doc.data(), doc.id);
+      setDog(doc.data())
+      console.log('dog, doc.data(): ', dog);
+    });
   return (
     <div>
+      <table>
+      <tr>
+        <th>Name</th>
+        <th>Age</th>
+        <th>Weight</th>
+        <th>Breed</th>
+      </tr>
+      <tr>
+        <th>{dog.dogName}</th>
+        <th>{dog.age}</th>
+        <th>{dog.weight} lbs</th>
+        <th>{dog.breed}</th>
+      </tr>
+      </table>
       <Hourly />
       <Daily />
     </div>
